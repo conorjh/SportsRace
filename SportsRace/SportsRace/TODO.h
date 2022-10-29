fb23257dae2646e47b154e65ed3a1c49bdb3d7fd
conorjh <conor.haddock@gmail.com>
2022-10-29 22:43:30 +0100

    Update TODO.h

--- SportsRace/SportsRace/TODO.h
+++ SportsRace/SportsRace/TODO.h
@@ -1,11 +1,16 @@
 /*
 Demo 2 Todo
-- Random names for racers
-- RacerDB
++ Random names for racers 
++ RacerDB
 - Better animation for runners
 - Race sounds
 - better league table, scrollable
 - varying speeds
 - racer view screen
 - basic career mode screen
+	- training
+	- race for cash
+	- race finder
+	- racer screen
+- cleanup renderer
 */
