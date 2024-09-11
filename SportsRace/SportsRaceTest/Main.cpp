ce48696ed79a0380cdb75aa2fff6c35eda5bd4bb
conorjh <conor.haddock@gmail.com>
2024-09-11 10:10:57 +0100

    add main to test

---
+++ SportsRace/SportsRaceTest/Main.cpp
@@ -0,0 +1,13 @@
+#include "App.h"
+#include "AppRenderer.h"
+
+using namespace Game;
+using namespace Game::App;
+using namespace Game::App::Renderer;
+
+int main(int argc, char* argv[])
+{
+	
+
+	return 0;
+}
