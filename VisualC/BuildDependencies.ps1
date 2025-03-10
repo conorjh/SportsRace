b51a76552a4f6191c15053045e9135e0f9285d6e
User <User@SCOOSIBOY>
2025-03-10 22:38:19 +0000

    vis see

--- VisualC/BuildDependencies.ps1
+++ VisualC/BuildDependencies.ps1
@@ -1,10 +1,4 @@
-Write-Host "Create lib directory"
-if (Test-Path "lib") 	{Remove-Item -Path "lib" -Force -Recurse}
-$null = mkdir external\lib\Win32\Debug
-$null = mkdir external\lib\x64\Debug
-$null = mkdir external\lib\Win32\Release
-$null = mkdir external\lib\x64\Release
-$null = mkdir lib
+
 
 $VsPath = "$Env:ProgramFiles\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
 
@@ -39,32 +33,3 @@ Write-Host "Building SDL_ttf"
 & "$VsPath" ".\external\SDL_ttf\VisualC\SDL_ttf.sln" /build "Release|Win32" | Out-Null
 & "$VsPath" ".\external\SDL_ttf\VisualC\SDL_ttf.sln" /build "Debug|x64"  | Out-Null
 & "$VsPath" ".\external\SDL_ttf\VisualC\SDL_ttf.sln" /build "Release|x64" | Out-Null
-
-Write-Host "Moving output to lib"
-$null = move external\SDL\VisualC\Win32\Debug\*.* external\lib\Win32\Debug
-$null = move external\SDL\VisualC\x64\Debug\*.* external\lib\x64\Debug
-$null = move external\SDL\VisualC\Win32\Release\*.* external\lib\Win32\Release
-$null = move external\SDL\VisualC\x64\Release\*.* external\lib\x64\Release
-
-$null = move external\SDL_image\VisualC\Win32\Debug\*.* external\lib\Win32\Debug
-$null = move external\SDL_image\VisualC\x64\Debug\*.* external\lib\x64\Debug
-$null = move external\SDL_image\VisualC\Win32\Release\*.* external\lib\Win32\Release
-$null = move external\SDL_image\VisualC\x64\Release\*.* external\lib\x64\Release
-
-$null = move external\SDL_mixer\VisualC\Win32\Debug\*.* external\lib\Win32\Debug
-$null = move external\SDL_mixer\VisualC\x64\Debug\*.* external\lib\x64\Debug
-$null = move external\SDL_mixer\VisualC\Win32\Release\*.* external\lib\Win32\Release
-$null = move external\SDL_mixer\VisualC\x64\Release\*.* external\lib\x64\Release
-
-$null = move external\SDL_net\VisualC\Win32\Debug\*.* external\lib\Win32\Debug
-$null = move external\SDL_net\VisualC\x64\Debug\*.* external\lib\x64\Debug
-$null = move external\SDL_net\VisualC\Win32\Release\*.* external\lib\Win32\Release
-$null = move external\SDL_net\VisualC\x64\Release\*.* external\lib\x64\Release
-
-$null = move external\SDL_ttf\VisualC\Win32\Debug\*.* external\lib\Win32\Debug
-$null = move external\SDL_ttf\VisualC\x64\Debug\*.* external\lib\x64\Debug
-$null = move external\SDL_ttf\VisualC\Win32\Release\*.* external\lib\Win32\Release
-$null = move external\SDL_ttf\VisualC\x64\Release\*.* external\lib\x64\Release
-
-Move-Item  -Path 'external\lib\*' -Destination 'lib' 
-Remove-Item -Path "external\lib" -Force -Recurse
--- VisualC/INSTALL.bat
+++ VisualC/INSTALL.bat
@@ -1,3 +1,2 @@
 powershell.exe ".\DownloadGit.ps1"
 powershell.exe ".\BuildDependencies.ps1"
-powershell.exe ".\CopyOutput.ps1"
